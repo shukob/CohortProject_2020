e6f595ee0e2b6cdf6ec064117e6399782402b3ea
Shumpei Kobayashi <skonb@me.com>
2020-07-26 05:53:24 +0900

    Progress

--- Project_3_Franck_Condon_Factors/lorentzian_smoothing.py
+++ Project_3_Franck_Condon_Factors/lorentzian_smoothing.py
@@ -1,17 +1,13 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.optimize import leastsq
+from scipy.optimize import least_squares
 
 
-def lorentzian(x, x0, a, gam):
+def lorentzian(x, x0, a, gam):  # gam = 1/Γ
     return a * gam ** 2 / (gam ** 2 + (x - x0) ** 2)
 
 
 def multi_lorentz(x, params):
-    off = params[0]
-    paramsRest = params[1:]
-    assert not (len(paramsRest) % 3)
-    return off + sum([lorentzian(x, *paramsRest[i: i + 3]) for i in range(0, len(paramsRest), 3)])
+    return sum([lorentzian(x, *params[i: i + 3]) for i in range(0, len(params), 3)])
 
 
 def res_multi_lorentz(params, xData, yData):
@@ -19,25 +15,30 @@ def res_multi_lorentz(params, xData, yData):
     return diff
 
 
-def smooth(xData, yData, width=1):
-    yData = yData / max(yData)
+def smooth(xData, yData, width=1, min_peaks=3, max_peaks=6, initial_guess_x=0, x0_step=1, ftol=1e-3, xtol=1e-3,
+           gtol=1e-3):
+    scale = max(yData)
+    yData = yData / scale
 
-    width = 1
-
-    yDataLoc = yData
-    startValues = [max(yData)]
+    offset = min(yData)
     counter = 0
 
-    while max(yDataLoc) - min(yDataLoc) > .1:
+    residue = [y - offset for y in yData]
+    optimization_result = None
+    initial_params = []
+    x0 = initial_guess_x
+    while max(residue) > .1:
         counter += 1
-        if counter > 20:  ### max 20 peak...emergency break to avoid infinite loop
+        if counter > max_peaks:
             break
-        minP = np.argmin(yDataLoc)
-        minY = yData[minP]
-        x0 = xData[minP]
-        startValues += [x0, minY - max(yDataLoc), width]
-        popt, ier = leastsq(res_multi_lorentz, startValues, args=(xData, yData))
-        yDataLoc = [y - multi_lorentz(x, popt) for x, y in zip(xData, yData)]
+        max_res = np.argmax(residue)
+        max_y = yData[max_res]
+        initial_params += [x0, max_y - residue[max_res], width]
+        optimization_result = least_squares(res_multi_lorentz, initial_params, args=(xData, yData), ftol=ftol,
+                                            xtol=xtol,
+                                            gtol=gtol, method='lm')
+        residue = [y - multi_lorentz(x, optimization_result.x) for x, y in zip(xData, yData)]
+        x0 += x0_step
 
-    return yDataLoc
-
+    data = [offset + multi_lorentz(x, optimization_result.x) for x in xData]
+    return data
